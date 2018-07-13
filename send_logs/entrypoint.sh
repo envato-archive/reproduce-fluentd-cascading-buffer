23ee514a49bfbdd363786eb99fe033ebd3a3e30d
Patrick Robinson <patrick.robinson@envato.com>
2018-07-13 15:43:24 +1000

    Limit events to 300 per second

--- send_logs/entrypoint.sh
+++ send_logs/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 while true; do
-    for i in {0..1000}; do
+    start=$(date +%s%6N)
+    end_at=$(expr $start + 1000000)
+    for i in {0..299}; do
         echo "123456${i}:awesome"
     done
+    end=$(date +%s%6N)
+    sleep_interval=$(echo "scale=6; ($end_at - $end) / 1000000" | bc)
+    sleep $sleep_interval
+    echo "$(date +%s) Sent 300 events" >> log.out
 done
